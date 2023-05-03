1b8cbf696dafc46d2445fb5e54a3afef1750c579
Rosie <rosie@greyhoundcs.com>
2023-05-03 12:25:16 +0100

    fixed minor merge errors

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from application.models import Users
+from application.models import User
 from flask import request, jsonify, render_template, redirect, url_for
 
 @app.route('/')
@@ -10,10 +10,10 @@ def welcome():
 @app.route('/users')
 def index():
     list = []
-    users = Users.query.all()
+    users = User.query.all()
     for user in users:
         data = {
-            "id": user.id,
+            "id": user.user_id,
             "username": user.username,
             "email": user.email,
             "password": user.password
@@ -31,7 +31,7 @@ def create_user():
     password = data.get('password')
     if not username or not email or not password:
         return jsonify({'error': 'Missing parameters'}), 400
-    user = Users(username=username, email=email, password=password)
+    user = User(username=username, email=email, password=password)
     db.session.add(user)
     db.session.commit()
     return jsonify({'message': 'Successfully created new user'}), 201
