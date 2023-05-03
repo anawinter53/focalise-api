310f07191fec21158aba5b0495d3f8f01fe848d7
anawinter53 <anatolia.winter@yahoo.com>
2023-05-03 11:20:43 +0100

    add task and user settings tables

--- application/models.py
+++ application/models.py
@@ -3,12 +3,56 @@ from application import app, db
 app.app_context().push()
 
 class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
 
-def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.password = password
+    settings = db.relationship('UserSettings', back_populates='user')
+    tasks = db.relationship('Tasks', back_populates='user')
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+class UserSettings(db.Model):
+    __tablename__ = 'settings'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    colour_scheme = db.Column(db.Integer, nullable=False, default=2)
+    push_notifications = db.Column(db.Integer, nullable=False, default=2)
+    points = db.Column(db.Integer, nullable=False, default=0)
+
+    user = db.relationship('Users', back_populates='settings')
+
+    def __init__(self, user_id, colour_scheme, push_notifications, points):
+        self.user_id = user_id
+        self.colour_scheme = colour_scheme
+        self.push_notifications = push_notifications
+        self.points = points
+
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_name = db.Column(db.String(30), nullable=False, default='Uncategorised')
+    task_name = db.Column(db.String(30), nullable=False)
+    task_url = db.Column(db.String(300), default='None')
+    task_desc = db.Column(db.String(100), nullable=False)
+
+    user = db.relationship('Users', back_populates='tasks')
+
+    def __init__(self, user_id, category_name, task_name, task_url, task_desc):
+        self.user_id = user_id
+        self.category_name = category_name
+        self.task_name = task_name
+        self.task_url = task_url
+        self.task_desc = task_desc
+    
+
+
+
+
+
--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 from application import db, app
-from application.models import Users
+from application.models import Users, UserSettings, Tasks
 
 def delete_database():
     with app.app_context():
@@ -10,18 +10,35 @@ def create_database():
         db.create_all()
 
 def add_entries():
-    entry1 = Users(username='admin', email='admin@admin.com', password='admin')
-    entry2 = Users(username='test', email='test@test.com', password='test')
+    Users1 = Users(username='admin', email='admin@admin.com', password='admin')
+    Users2 = Users(username='test', email='test@test.com', password='test')
+    Settings1 = UserSettings(user_id=1, colour_scheme=3, push_notifications=2, points=1000)
+    Settings2 = UserSettings(user_id=2, colour_scheme=2, push_notifications=1, points=50)
+    Tasks1 = Tasks(user_id=1, category_name='Python', task_name='Python testing', task_url='https://realpython.com/python-testing/', task_desc='Try and understand the concept of unit testing with Python')
+    Tasks2 = Tasks(user_id=1, category_name='Django', task_name='Django intro', task_url='https://www.w3schools.com/django/django_intro.php', task_desc='Start on Django')
+    Tasks3 = Tasks(user_id=1, category_name='Design', task_name='Design plans', task_url='None', task_desc='Find some designs for a portfolio website')
+
     with app.app_context():
-        db.session.add(entry1)
-        db.session.add(entry2)
+        db.session.add(Users1)
+        db.session.add(Users2)
+        db.session.add(Settings1)
+        db.session.add(Settings2)
+        db.session.add(Tasks1)
+        db.session.add(Tasks2)
+        db.session.add(Tasks3)
         db.session.commit()
 
 def see_db_entries():
     with app.app_context():
-        entries = Users.query.all()
-        for entry in entries:
-            print(f"{entry.username}, {entry.email}, {entry.password}")
+        users = Users.query.all()
+        for user in users:
+            print(f"{user.username}, {user.email}, {user.password}")
+        settings = UserSettings.query.all()
+        for setting in settings:
+            print(f"{setting.colour_scheme}")
+        tasks = Tasks.query.all()
+        for task in tasks:
+            print(f"{task.category_name}, {task.task_name}, {task.task_desc}")
 
 if __name__ == '__main__':
     delete_database()
