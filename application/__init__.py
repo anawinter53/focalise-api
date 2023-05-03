597b989eeef0a644176ae413c64038fddfd3c90a
anawinter53 <anatolia.winter@yahoo.com>
2023-05-03 09:16:45 +0100

    finally get database up and running

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 
-
-# app.config.from_pyfile('settings.py')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nmtpqjvn:cbsEDgvmrjI52_hpEb7hkSsdxjYSgEDs@horton.db.elephantsql.com/nmtpqjvn'
-#the following line's value can be anything, it's just a password of your choice
-app.config['SECRET_KEY'] = '0e82eec5faf1fb25c1143590575f7a60'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
--- application/models.py
+++ application/models.py
@@ -1,5 +1,7 @@
 from application import app, db
 
+app.app_context().push()
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
